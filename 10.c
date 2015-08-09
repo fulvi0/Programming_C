51c0424e4a072faf4e9e68f1dbd30ffce802819f
Carlos F. Antonio <fulvio.ac@gmail.com>
2015-08-09 17:24:38 -0400

    trabajando tarea no. 10

--- 10.c
+++ 10.c
@@ -3,5 +3,25 @@
   y al final imprima los numeros
   del 1 al 15 menos los numeros capturados
 */
-
 #include <stdio.h>
+
+int main() {
+
+   int array[15], position, c, n;
+   int a,b,c;
+
+   if ( position >= n+1 )
+      printf("Deletion not possible.\n");
+   else
+   {
+      for ( c = position - 1 ; c < n - 1 ; c++ )
+         array[c] = array[c+1];
+
+      printf("Resultant array is\n");
+
+      for( c = 0 ; c < n - 1 ; c++ )
+         printf("%d\n", array[c]);
+   }
+
+   return 0;
+}
