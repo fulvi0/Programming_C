ddb680bc576400c5fe8e33c4033a3820655c7d8c
Carlos F. Antonio <fulvio.ac@gmail.com>
2015-08-09 20:21:40 -0400

    working on program que pida 3 letras y al final imprima cuantas repeticiones hay

--- 17.c
+++ 17.c
@@ -3,3 +3,21 @@
 */
 
 #include <stdio.h>
+
+int main(){
+
+  char c[3], x;
+  int i, count=0;
+
+  printf("Enter a string: ");
+    for (x = 0; x <= 3; x++)
+      scanf("%c", &c[x] );
+
+    for(i=0;c[i]!='\0';++i){
+      if(c[x]==c[i])
+        ++count;
+    }
+    printf("Frequency of %c = %d", x, count);
+
+  return 0;
+}
